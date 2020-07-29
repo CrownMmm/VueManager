8c34a190975292a992cafb5e68259249902ac252
zhangjiaming <823600537@qq.com>
2020-07-29 20:33:48 +0800

    配置eslint语法规范

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-      home1
+      <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
