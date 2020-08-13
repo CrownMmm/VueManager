c8d5966fe24451d33bdc86b32b2d9388d1509c0f
zhangjiaming <823600537@qq.com>
2020-08-14 01:31:26 +0800

    feat:添加order基本样式

---
+++ src/components/order/Order.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 订单列表查询参数
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      total: 0,
+      orderList: []
+    }
+  },
+  created () {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList () {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败！')
+      }
+      this.total = res.data.total
+      this.orderList = res.data.goods
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
