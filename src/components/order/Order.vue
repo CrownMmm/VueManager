3193dc1c25453d95067dee6921bb679c89f645fc
zhangjiaming <823600537@qq.com>
2020-08-14 01:48:30 +0800

    feat:完成order列表以及分页功能

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -15,6 +15,38 @@
           </el-input>
         </el-col>
       </el-row>
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="danger" v-if="scope.row.pay_status === '0'">未付款</el-tag>
+            <el-tag type="success" v-else>已付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">{{scope.row.create_time | dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" size="mini" icon="el-icon-edit"></el-button>
+            <el-button type="success" size="mini" icon="el-icon-location"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -47,6 +79,15 @@ export default {
       this.total = res.data.total
       this.orderList = res.data.goods
       console.log(res)
+    },
+    // 分页
+    handleSizeChange (newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange (newSize) {
+      this.queryInfo.pagenum = newSize
+      this.getOrderList()
     }
   }
 }
