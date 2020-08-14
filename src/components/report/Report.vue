f73ee2e9e37381512d40b293a1858c4b7cdda03e
zhangjiaming <823600537@qq.com>
2020-08-15 02:08:35 +0800

    feat:完成echarts 基本用法

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -17,6 +17,8 @@
 <script>
 // 导入echarts
 import echarts from 'echarts'
+import _ from 'lodash'
+
 export default {
   data () {},
   created () {},
@@ -31,28 +33,28 @@ export default {
 
     // 4.准备数据项和配置项
     // 指定图表的配置项和数据
-    var option = {
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量']
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子', '帽子']
-      },
-      yAxis: {},
-      series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20, 1]
-      }]
-    }
+    // var option = {
+    //   title: {
+    //     text: 'ECharts 入门示例'
+    //   },
+    //   tooltip: {},
+    //   legend: {
+    //     data: ['销量']
+    //   },
+    //   xAxis: {
+    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子', '帽子']
+    //   },
+    //   yAxis: {},
+    //   series: [{
+    //     name: '销量',
+    //     type: 'bar',
+    //     data: [5, 20, 36, 10, 10, 20, 1]
+    //   }]
+    // }
     // 数据合并
-    // const result = _.merge(res.data, this.options)
+    const result = _.merge(res.data, this.options)
     // 5.展示数据
-    myChart.setOption(option)
+    myChart.setOption(result)
   },
   methods: {}
 }
